8b709e168ae174d0378cd0efee68c4a4fb5d97bf
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-11 12:45:12 +0500

    09

--- bool09.py
+++ bool09.py
@@ -1,10 +1,10 @@
 def main(a):
     """Check the natural number.Natural numbers are numbers used in counting.
     Args:
-        a: int
+        a: int 
     Returns:
         bool
     """
     # Write your code here
-    return bool((a/1>0))
-print(main(-1))
+    return a//1>0 and a>0
+print(main(2.3))     
